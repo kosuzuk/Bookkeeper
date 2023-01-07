451d56dd6ce3646ea09f755cd8baaa04f9f3d590
Koso Suzuki <koso.suzuki@interlochen.org>
2023-01-07 14:44:13 +0900

    Initial Commit

---
+++ Bookkeeper/BookkeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookkeeperApp.swift
+//  Bookkeeper
+//
+//  Created by Koso Suzuki on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BookkeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
